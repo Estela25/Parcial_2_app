249e95df652825e0a133d0b5e72f7628dc4aa4ec
Estela25 <USIS035821@ugb.edu.sv>
2022-04-24 12:42:02 -0500

    arreglos

--- app/src/main/java/com/ugb/miprimerproyecto/Ayuda.java
+++ app/src/main/java/com/ugb/miprimerproyecto/Ayuda.java
@@ -2,7 +2,11 @@ package com.ugb.miprimerproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
 
 public class Ayuda extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class Ayuda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda);
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        String path = "android.resource://"+ getPackageName() + "/" +
+                R.raw.;
+        videoView.setVideoURI(Uri.parse(path));
+        videoView.start();
+    }
+
+    public void regresar(View view) {
+        Intent volver = new Intent(this, MainActivity.class);
+        startActivity(volver);
     }
 }
